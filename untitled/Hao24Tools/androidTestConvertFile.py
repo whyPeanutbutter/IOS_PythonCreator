1a955823d48b286959c63cbc09c81355d553b27f
wuhy <1094405395@qq.com>
2022-07-08 14:47:08 +0800

    Update androidTestConvertFile.py

--- untitled/Hao24Tools/androidTestConvertFile.py
+++ untitled/Hao24Tools/androidTestConvertFile.py
@@ -89,6 +89,66 @@ def lower_str():
     window.clipboard_clear()
     window.clipboard_append(outStr)
 
+def lower_str():
+    nameEntry.insert('end', "")
+    tempstr = nameEntry.get('1.0', 'end')
+    if len(tempstr) == 0 or tempstr == '\n':
+        tempstr = window.clipboard_get()
+    tempstr = tempstr.replace('\t\n', '\n').replace('\t', ' ')
+    arr = tempstr.split("\n")
+    outStr = ""
+    notInferString = "以下是程序无法推断的属性或是不需要的注释:"
+    j = 0
+    for astr in arr:
+        nameEntry.insert('end', "")
+        j = j + 1
+        arrTemp = astr.split(" ")
+        arrTemp = list(filter(lambda a: a != '可选', arrTemp))
+        arrTemp = list(filter(lambda a: a != '必选', arrTemp))
+        arrTemp = list(filter(lambda a: a != '', arrTemp))
+        needJump = False
+        if len(arrTemp) == 0 or arrTemp[0] == '参数':
+            needJump = True
+        if not needJump:
+            protertyName = arrTemp[0][0:1].lower() + arrTemp[0][1:]
+            orginArrTemp = arrTemp.copy()
+            for i in range(len(arrTemp)):
+                arrTemp[i] = arrTemp[i].lower()
+            if len(arrTemp) > 1:
+                if "string" in arrTemp:
+                    outStr = outStr + '\n' + "private String " + protertyName + ';'
+                elif ("int" in arrTemp) or ("long" in arrTemp) or ("datetime" in arrTemp) or ("number" in arrTemp) or (
+                        "integer" in arrTemp) or ("date" in arrTemp):
+                    outStr = outStr + '\n' + "private Integer " + protertyName + ';'
+                elif ("bool" in arrTemp) or ("boolean" in arrTemp):
+                    outStr = outStr + '\n' + "private Boolean " + protertyName + ';'
+                elif ("double" in arrTemp) or ("float" in arrTemp):
+                    outStr = outStr + '\n' + "private Float " + protertyName + ';'
+                elif arrTemp[1].startswith('list<'):
+                    outStr = outStr + '\n' + "private List<EL" + orginArrTemp[1][5:-1] + ' ' + "> " + protertyName + ';(这是推测类型)'
+                else:
+                    outStr = outStr + '\n' + "private " + orginArrTemp[1] + ' ' + protertyName + ';(这是推测类型)'
+            else:
+                print(outStr)
+                print(arrTemp)
+                if arrTemp[0] == 'success':
+                    continue
+                elif len(outStr) == 0:
+                    outStr = "public class EL" + protertyName[0:1].capitalize() + protertyName[1:] + ' { (这是推测类型)'
+                else:
+                    outStr = outStr + "\n}\n" + '\n\n' + "public class EL" + strTemp[0:1].capitalize() + strTemp[
+                                                                                                          1:] + ' { (这是推测类型)'
+        if needJump == False and len(arrTemp) > 2 and is_first_chinese_or_number(arrTemp[2]):
+            outStr = outStr + '//' + arrTemp[2]
+        if j == len(arr):
+            if notInferString != "以下是程序无法推断的属性或是不需要的注释:":
+                outStr = outStr + "\n" + notInferString
+            nameEntry.insert('end', "")
+            nameEntry.delete('1.0', 'end')
+            nameEntry.insert('end', outStr)
+            window.clipboard_clear()
+            window.clipboard_append(outStr)
+            return
 
 def get_firist():
     tempstr = nameEntry.get('1.0', 'end')
